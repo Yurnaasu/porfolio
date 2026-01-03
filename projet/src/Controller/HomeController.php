0f5e978577df012cd3702d04875354a366f1a0e3
Yurnaasu <manal.ferdaoui@etu.univ-st-etienne.fr>
2026-01-03 18:27:46 +0100

    Correction symphony

--- projet/src/Controller/HomeController.php
+++ projet/src/Controller/HomeController.php
@@ -4,15 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    /* Ajoute ces blocs pour créer les liens vers tes autres pages */
+    
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('home/index.html.twig'); 
+    }
+
+    #[Route('/competence', name: 'app_competence')]
+    public function competence(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/index.html.twig');
     }
 }
